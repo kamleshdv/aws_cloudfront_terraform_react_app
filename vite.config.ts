719d8e228fcaace96df6035e190889cdcb6a03cc
kamleshdv <114006749+kamleshdv@users.noreply.github.com>
2026-06-04 14:31:56 +0530

    Update Vite config for AWS CloudFront and output directory

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/aws_cloudfront_terraform_react_app/',  // REPO NAME - IMPORTANT!
   build: {
-    outDir: 'build',
+    outDir: 'dist',  // GitHub Pages expects 'dist' by default
     sourcemap: true,
   },
   test: {
